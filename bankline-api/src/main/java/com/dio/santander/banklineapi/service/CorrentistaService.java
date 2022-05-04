099257d1285fbe6001890207e2d09e12cd761007
alinealien <alinebdantunes@gmail.com>
2022-05-04 18:45:07 -0300

    Manipulando os dados dos correntistas

--- bankline-api/src/main/java/com/dio/santander/banklineapi/service/CorrentistaService.java
+++ bankline-api/src/main/java/com/dio/santander/banklineapi/service/CorrentistaService.java
@@ -1,6 +1,7 @@
 package com.dio.santander.banklineapi.service;
 import java.util.Date;
 
+//SERVICE - Regra de negocios
 import com.dio.santander.banklineapi.dto.NovoCorrentista;
 import com.dio.santander.banklineapi.model.Conta;
 import com.dio.santander.banklineapi.model.Correntista;
@@ -8,7 +9,7 @@ import com.dio.santander.banklineapi.repository.CorrentistaRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+//Componente do tipo servico do spring boot
 @Service
 public class CorrentistaService {
     @Autowired
